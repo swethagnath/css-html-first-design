66bc0002865621cba4c5d1fa610f0530df1ad56f
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-04-12 21:50:42 +0530

    created purehtml-css-responsive

---
+++ main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function () {
+    $('.fa-bars').click(function () {
+        $('.fa-bars').toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+        if ($(window).scrollTop() > 30) {
+            $('.header').addClass('header-active');
+        } else {
+            $('.header').removeClass('header-active');
+        }
+
+        $('section').each(function () {
+            let id = $(this).attr('id');
+            let height = $(this).height();
+            let offset = $(this).offset.top - 200;
+            let top = $(window).scrollTop();
+            if (top >= offset && top < height + offset) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(('[href="#' + id + '"]')).addClass('active');
+            }
+        })
+    })
+
+
+});
